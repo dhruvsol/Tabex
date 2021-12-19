f9e08cbeda1d0f08ded8ba1f2fc5abe88248edf6
prykdev <priyankaafssulur@gmail.com>
2021-12-19 12:47:26 +0530

    Added card and navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {AdminLogin} from "./components/admins/adminLogin/AdminLogin"
 import {AdminSignup} from "./components/admins/adminSignup/AdminSignup"
 import {UserSignup} from "./components/user/userSignup/UserSignup"
 import {UserLogin} from "./components/user/userLogin/UserLogin"
+import { Restaurant  } from "./components/user/Restaurant/Restaurant";
+import { Cards } from "./components/user/Restaurant/Cards";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
     <Route path="/admin/Login" element={<AdminLogin/>}/>
     <Route path="/user/Login" element={<UserLogin/>}/>
     <Route path="/user/signup" element={<UserSignup/>}/>
+    <Route path="/user/Restaurant" element={<Restaurant/>}/>
+    <Route path="/user/Restaurant/Cards" element={<Cards/>}/>
   </Routes>
   </BrowserRouter>
  

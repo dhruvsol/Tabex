abf2d0a51f237eb90fa624261b4169a60b556eb0
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-19 18:43:56 +0530

    Revert "no idea"
    This reverts commit e25fdb8eac15a4630c5961fba995fa34076b6bd9.

--- src/components/admins/adminSignup/AdminSignup.jsx
+++ src/components/admins/adminSignup/AdminSignup.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import { supabase } from '../../../supabaseClient';
 export const AdminSignup = () => {
    // eslint-disable-next-line
-    const [username, setUsername] = useState("");   
+    // const [username, setUsername] = useState("");   
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");    
     const clicked = async()=>{
@@ -11,13 +11,6 @@ export const AdminSignup = () => {
         email: email,
         password: password,
       })
-      // const {data} =await supabase 
-      // .from('profiles')
-      // .eq('id',user?.id)
-      // .single();
-      // if(data){
-      //   setUsername(data.username)
-      // }
 
     
     }    
@@ -30,12 +23,12 @@ export const AdminSignup = () => {
           <a href="/admin/login">Already Have an Account ?</a>
         </div>
         <div className="inputs">
-            <input
+            {/* <input
           className="input"
             type="username"
             placeholder="Username...."
             onChange={(e) => setUsername(e.target.value)}
-          />
+          /> */}
           <input
           className="input"
             type="email"
